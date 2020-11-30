68960ad25274fb4f34ec40cb185e81e4f40e824e
Mikah Niehaus <Graywolf.niehaus@live.com>
2020-11-30 12:11:10 -0600

    added some tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
     <div data-test="component-app">
-    
+    <h1 data-test="counter-display">The counter is working</h1>
+    <button data-test='increment-button' >Counter</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+import React from 'react';
+import App from './App.js';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 
 
 Enzyme.configure({adapter: new EnzymeAdapter()})
+/**
+ * factery function to creat ShallowWrapper for the App component
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
 
 test('renders without error', () => {
-  const wrapper = shallow(<App/>);
-  const appComponent = wrapper.find("[data-test='component-app']")
-  expect(appComponent.length).toBe(1)
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders button", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
+  expect(button.length).toBe(1);
+});
+
+test("renders counter display", () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
 });
